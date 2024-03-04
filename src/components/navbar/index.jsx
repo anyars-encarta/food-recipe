d6012c7490844766bd77a34fde5926a46efe010b
anyars-encarta <anyarsencarta@gmail.com>
2024-03-04 08:49:08 +0000

    Implement Navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,8 +1,29 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar Component
-        </div>
+        <nav className='flex justify-between item-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0'>
+            <h2 className='text-2xl font-semibold'>
+                <NavLink to={'/'} className='text-black hover:text-gray-700 duration-300'>Food Recipe</NavLink>
+            </h2>
+
+            <form>
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Search recipe..."
+                    value=""
+                    className='bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200' />
+            </form>
+            <ul className='flex gap-5'>
+                <li>
+                    <NavLink to={'/'} className='text-black hover:text-gray-700 duration-300'>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/favorites'} className='text-black hover:text-gray-700 duration-300'>Favorites</NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
