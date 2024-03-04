4145a1f8561f59b6b4734f543af6277dd312dc5a
anyars-encarta <anyarsencarta@gmail.com>
2024-03-04 18:09:18 +0000

    Correct eslint error

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import Home from './pages/home';
 import Details from './pages/details';
 import Favorites from './pages/favorites';
 
-const App = () => {
-  return (
-    <div>
-      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
-        <Navbar />
+const App = () => (
+  <div>
+    <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/recipe-item/:id" element={<Details />} />
-        </Routes>
-      </div>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/recipe-item/:id" element={<Details />} />
+      </Routes>
     </div>
-  );
-}
+
+  </div>
+);
 
 export default App;
